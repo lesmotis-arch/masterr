8abc4937497fb83f124b521797f494142552b115
lesmotis-arch <lesmotis@gmail.com>
2026-05-11 20:38:14 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -214,3 +214,19 @@ window.addEventListener("scroll", () => {
   });
 
 });
+// =========================
+// Кнопки FAQ
+// =========================
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach(item => {
+
+    const button = item.querySelector(".faq-question");
+
+    button.addEventListener("click", () => {
+
+        item.classList.toggle("active");
+
+    });
+
+});
